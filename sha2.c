94a4e53012c5d4e9a0b04213cb2723946f4fe38b
Rogerio Alves <rogealve@br.ibm.com>
2016-12-19 10:31:55 -0600

    Moving constants to headers and call sha from main.
    Summary:
    
    It's pointless to create a function sha2 just to pass argc
    and argv from man and then call another function sha_core.
    Let's simplify things by rename sha_core do sha2, call
    directly from main and deal with things like open file and
    buffer allocation in main.
    
    Also this commit moves constants definition to a own header
    and declare _h as extern so we can define it where it's used
    get rid of a 'unused variable' warning.

--- sha2.c
+++ sha2.c
@@ -6,7 +6,45 @@
 #include "sha2.h"
 
 int main (int argc, char *argv[]) {
-    return sha2(argc, argv);
+	char *filename = argv[1];
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL) {
+      fprintf(stderr, "Error while open %s. %s\n", argv[1],
+        strerror(errno));
+      return errno;
+    }
+
+    // Get file size
+    struct stat st;
+
+    if (stat(argv[1], &st) != 0) {
+      fprintf(stderr, "Cannot determine size of %s: %s\n", argv[1],
+        strerror(errno));
+      fclose(file);
+      return errno;
+    }
+
+	// Padding. padded_size is total message bytes including pad bytes.
+	size_t padded_size = calculate_padded_msg_size(st.st_size);
+
+	// Save file in a input buffer.
+	char* input = (char *) malloc(padded_size);
+    if (input == NULL) {
+      fprintf(stderr, "%s\n.", strerror(errno));
+      return errno;
+    }
+
+	if (fread(input, sizeof(char), st.st_size, file) != st.st_size) {
+      fprintf(stderr, "Read error on file %s. %s\n", argv[1],
+        strerror(errno));
+      fclose(file);
+	  return errno;
+	}
+
+	fclose(file);
+
+	return sha2(input, st.st_size, padded_size, _h);
 }
 
 // if using vim with "set modeline" on your .vimrc, then this file will be
--- sha2.h
+++ sha2.h
@@ -7,91 +7,23 @@
 
 #include <sys/stat.h>
 
+#include "base-types.h"
+
+#if SHA_BITS == 256
+base_type _h[8] = {
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+};
+#elif SHA_BITS == 512
+base_type _h[8] = {
+    0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
+    0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
+};
+#endif
+
 #if (LOW_LEVEL == 2 || LOW_LEVEL == 1) && !defined(__powerpc64__)
 	#error "HW vector only implemented for powerpc64"
 #endif
-
-#if SHA_BITS == 256
-
-typedef uint32_t base_type;
-typedef vector unsigned int vector_base_type;
-
-static base_type _h[8] = {
-	0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-	0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
-};
-static const base_type k[64] = {
-	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-	0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-	0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-	0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-	0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-	0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-	0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-};
-static const size_t W_SIZE     = 64;
-static const size_t BLOCK_SIZE = 64;
-
-static const size_t PAD_MOD = 512;
-static const size_t PAD_EQL = 448;
-
-static const base_type S0_args[] = { 2, 13, 22};
-static const base_type S1_args[] = { 6, 11, 25};
-static const base_type s0_args[] = { 7, 18,  3};
-static const base_type s1_args[] = {17, 19, 10};
-
-#elif SHA_BITS == 512
-
-typedef uint64_t base_type;
-typedef vector unsigned long vector_base_type;
-
-static base_type _h[8] = {
-	0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
-	0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
-};
-static const base_type k[80] = {
-	0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538,
-	0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe,
-	0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235,
-	0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
-	0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5, 0x983e5152ee66dfab,
-	0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725,
-	0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed,
-	0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
-	0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218,
-	0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53,
-	0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373,
-	0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
-	0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c,
-	0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6,
-	0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc,
-	0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
-};
-static const size_t W_SIZE     = 80;
-static const size_t BLOCK_SIZE = 128;
-
-static const size_t PAD_MOD    = 1024;
-static const size_t PAD_EQL    = 896;
-
-static const base_type S0_args[] = {28, 34, 39};
-static const base_type S1_args[] = {14, 18, 41};
-static const base_type s0_args[] = { 1,  8,  7};
-static const base_type s1_args[] = {19, 61,  6};
-
-#else
-#error "Invalid SHA_BITS"
-#endif
-
-static const size_t base_type_size = sizeof(base_type);
 
 base_type calc_ch(base_type e, base_type f, base_type g) {
 	base_type tmp, tmp2;
@@ -194,7 +126,7 @@ void write_size(char *input, size_t size, size_t position) {
 	*(++total_size) = (base_type)size * 8;                   // lower bits
 }
 
-void sha2_core(char *input, size_t size, size_t padded_size, base_type *_h) {
+int sha2(char *input, size_t size, size_t padded_size, base_type *_h) {
 	// Pad trailing bytes accordingly.
 	pad(input, size, padded_size);
 
@@ -202,7 +134,7 @@ void sha2_core(char *input, size_t size, size_t padded_size, base_type *_h) {
     char* input_swapped = (char *) malloc(padded_size);
     if (input_swapped == NULL) {
       fprintf(stderr, "%s\n.", strerror(errno));
-      exit(EXIT_FAILURE);
+      return errno;
     }
 	swap_bytes(input, input_swapped, padded_size);
 
@@ -215,48 +147,6 @@ void sha2_core(char *input, size_t size, size_t padded_size, base_type *_h) {
 		calculate_w_vector(w, input_swapped+i);
 		calc_compression(_h, w);
 	}
-}
-
-int sha2 (int argc, char *argv[]) {
-	char *filename = argv[1];
-	FILE *file = fopen(filename, "r");
-
-	if (file == NULL) {
-      fprintf(stderr, "Error while open %s. %s\n", argv[1],
-        strerror(errno));
-      return errno;
-    }
-
-    // Get file size
-    struct stat st;
-
-    if (stat(argv[1], &st) != 0) {
-      fprintf(stderr, "Cannot determine size of %s: %s\n", argv[1],
-        strerror(errno));
-      fclose(file);
-      return errno;
-    }
-
-	// Padding. padded_size is total message bytes including pad bytes.
-	size_t padded_size = calculate_padded_msg_size(st.st_size);
-
-	// Save file in a input buffer.
-	char* input = (char *) malloc(padded_size);
-    if (input == NULL) {
-      fprintf(stderr, "%s\n.", strerror(errno));
-      return errno;
-    }
-
-	if (fread(input, sizeof(char), st.st_size, file) != st.st_size) {
-      fprintf(stderr, "Read error on file %s. %s\n", argv[1],
-        strerror(errno));
-      fclose(file);
-	  return errno;
-	}
-
-	fclose(file);
-
-	sha2_core(input, st.st_size, padded_size, _h);
 
 	printf(
 #if SHA_BITS == 256
