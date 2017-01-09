0d02dec5f28755dc33ac5af2967e824a0a2e12d0
Rogerio Alves <rogealve@br.ibm.com>
2017-01-09 11:32:38 -0600

    Unrolling intrinsic variant.
    Summary:
    
    This commit changes intrinsic variant to use unrolling like
    c and asm variants.

--- sha2_ll_intrinsics.h
+++ sha2_ll_intrinsics.h
@@ -9,63 +9,38 @@
 
 #include "base-types.h"
 
-void sha2_transform(base_type* _h, base_type* w) {
-  vector_base_type bsigma; // big Sigma vector
-  vector_base_type sigma; // small sigma vector
-  vector_base_type w_vec, k_vec, kw_vec; // temp vectors to sum k+w
-  base_type a, b, c, d, e, f, g, h, tmp1, tmp2; // compression registers
+void static inline sha2_round(base_type* a, base_type* b, base_type* c,
+                              base_type* d, base_type* e, base_type* f,
+                              base_type* g, base_type* h, base_type k,
+                              base_type w) {
 
+  vector_base_type bsigma;
+  base_type tmp1, tmp2;
+
+  bsigma[0] = *a;
+  bsigma[1] = *e;
 #if SHA_BITS == 256
-  // Expand two message blocks per loop cycle
-  for (int t = 16; t < W_SIZE; t += 2) {
-    //sig0  = w[t-15];
-    //sig1  = w[t-2];
-    //sig0' = w[t-14];
-    //sig1' = w[t-1];
-    sigma = (vector_base_type) { w[t-15], w[t-2], w[t-14], w[t-1] };
-    sigma = __builtin_crypto_vshasigmaw(sigma, 0, 0xA);
-    w[t]   = sigma[1] + w[t-7] + sigma[0] + w[t-16];
-    w[t+1] = sigma[3] + w[t-6] + sigma[2] + w[t-15];
-    // Wt is added to Kt in compression function we can move this operation to
-    // message scheduler; This can be done since both Wt and Kt are available
-    // and there's no dependency with the others operands. Since we already
-    // have the operands avaliable here we can sum avoiding load those
-    // operands again on compression phase.
-    // TODO(rcardoso): we can try to sum the two values with one operation.
-    w[t-16] += k[t-16];
-    w[t-15] += k[t-15];
-  }
-
-  // Continue to sum Kt to Wt. Sums the last sixteen values in parallel using
-  // vector add operations (4x4). On asm code we can easily unroll this loop
-  // avoiding branchs.
-  for (int i = W_SIZE-16; i < W_SIZE; i += 4) {
-    w_vec = (vector_base_type) { w[i], w[i+1], w[i+2], w[i+3] };
-    k_vec = (vector_base_type) { k[i], k[i+1], k[i+2], k[i+3] };
-    kw_vec = __builtin_vec_add(w_vec, k_vec);
-    __builtin_vec_st(kw_vec, 0, &w[i]);
-  }
+  bsigma = __builtin_crypto_vshasigmaw(bsigma, 1, 0xE);
 #elif SHA_BITS == 512
-  for (int t = 16; t < W_SIZE; t++) {
-    // sig0 = w[t-15];
-    // sig1 = w[t-2];
-    sigma = (vector_base_type) {  w[t-15], w[t-2] };
-    sigma = __builtin_crypto_vshasigmad(sigma, 0, 0xD);
-    w[t]   = sigma[1] + w[t-7] + sigma[0] + w[t-16];
-    // Wt + Kt
-    w[t-16] += k[t-16];
-  }
-  // Sum the sixteen k to w (2x2)
-  for (int i = W_SIZE-16; i < W_SIZE; i += 2) {
-    w_vec = (vector_base_type) { w[i], w[i+1] };
-    k_vec = (vector_base_type) { k[i], k[i+1] };
-    kw_vec = __builtin_vec_add(w_vec, k_vec);
-    // TODO(rcardoso): Replace this for a vector instrinsic instructions.
-    // Note that we don't have a builtin to store long values.
-    w[i] = kw_vec[0];
-    w[i+1] = kw_vec[1];
-  }
+  bsigma = __builtin_crypto_vshasigmad(bsigma, 1, 0xD);
 #endif
+  tmp1 =  *h + bsigma[1] + Ch(*e, *f, *g) + w + k;
+  tmp2 = bsigma[0] + Maj(*a, *b, *c);
+
+  *h = *g;
+  *g = *f;
+  *f = *e;
+  *e = *d + tmp1;
+  *d = *c;
+  *c = *b;
+  *b = *a;
+  *a = tmp1 + tmp2;
+}
+
+void sha2_transform(base_type* _h, base_type* w) {
+  vector_base_type sigma; // small sigma vector
+  base_type a, b, c, d, e, f, g, h; // compression registers
+  uint32_t t;
 
   a = _h[0];
   b = _h[1];
@@ -76,26 +51,34 @@ void sha2_transform(base_type* _h, base_type* w) {
   g = _h[6];
   h = _h[7];
 
-  for (int i = 0; i < W_SIZE; i++) {
-    bsigma[0] = a;
-    bsigma[1] = e;
-#if SHA_BITS == 256
-    bsigma = __builtin_crypto_vshasigmaw(bsigma, 1, 0xE);
-#elif SHA_BITS == 512
-    bsigma = __builtin_crypto_vshasigmad(bsigma, 1, 0xD);
-#endif
-    tmp1 =  h + bsigma[1] + Ch(e, f, g) + w[i];
-    tmp2 = bsigma[0] + Maj(a, b, c);
-
-    h = g;
-    g = f;
-    f = e;
-    e = d + tmp1;
-    d = c;
-    c = b;
-    b = a;
-    a = tmp1 + tmp2;
+  for (t = 0; t < 16; t++) {
+    sha2_round(&a, &b, &c, &d, &e, &f, &g, &h, k[t], w[t]);
   }
+#if SHA_BITS == 256
+  // Expand two message blocks per loop cycle
+  for (; t < W_SIZE; t += 2) {
+    //sig0  = w[t-15];
+    //sig1  = w[t-2];
+    //sig0' = w[t-14];
+    //sig1' = w[t-1];
+    sigma = (vector_base_type) { w[t-15], w[t-2], w[t-14], w[t-1] };
+    sigma = __builtin_crypto_vshasigmaw(sigma, 0, 0xA);
+    w[t] = sigma[1] + w[t-7] + sigma[0] + w[t-16];
+    sha2_round(&a, &b, &c, &d, &e, &f, &g, &h, k[t], w[t]);
+    w[t+1] = sigma[3] + w[t-6] + sigma[2] + w[t-15];
+    sha2_round(&a, &b, &c, &d, &e, &f, &g, &h, k[t+1], w[t+1]);
+  }
+#elif SHA_BITS == 512
+  for (; t < W_SIZE; t++) {
+    //sig0  = w[t-15];
+    //sig1  = w[t-2];
+    sigma = (vector_base_type) { w[t-15], w[t-2] };
+    sigma = __builtin_crypto_vshasigmad(sigma, 0, 0xD);
+    w[t] = sigma[1] + w[t-7] + sigma[0] + w[t-16];
+    sha2_round(&a, &b, &c, &d, &e, &f, &g, &h, k[t], w[t]);
+  }
+#endif
+
   _h[0] += a;
   _h[1] += b;
   _h[2] += c;
@@ -105,5 +88,4 @@ void sha2_transform(base_type* _h, base_type* w) {
   _h[6] += g;
   _h[7] += h;
 }
-
 #endif // _PPC64_LE_SHA2_LL_INTRINSICS_H_
